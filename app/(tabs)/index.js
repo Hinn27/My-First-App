5197256aebbcfcb4394b5bb8a11043498ed6671e
Hinn <duc107243@donga.edu.vn>
2025-11-24 13:05:58 +0700

    Nothing

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,11 +1,10 @@
 // Screen: Index - Redirect to Home
 /* Chức năng:
  * File redirect mặc định cho tab root
- * Expo Router sẽ tự động chuyển hướng đến home.js
+ * Expo Router sẽ tự động chuyển hướng đến home.js (tương lai sẽ đem home qua đây, bỏ home.js)
  */
 import { Redirect } from 'expo-router';
 
 export default function Index() {
     return <Redirect href="/(tabs)/home" />;
 }
-
